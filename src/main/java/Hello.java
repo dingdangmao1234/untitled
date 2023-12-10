85b270e29c81dba74cee9499b860306380caab68
dingdangmao1234 <153421761+dingdangmao1234@users.noreply.github.com>
2023-12-10 17:08:59 +0800

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,6 +9,6 @@ public class Hello {
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("push test");
-
+        System.out.println("pull test");
     }
 }
