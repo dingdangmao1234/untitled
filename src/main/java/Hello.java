87f483086d40b24d6c7a1d5c30e31be0b74f0743
liulvsheng <13562881+liulvsheng@user.noreply.gitee.com>
2023-12-10 09:49:09 +0000

    update src/main/java/Hello.java.
    Signed-off-by: liulvsheng <13562881+liulvsheng@user.noreply.gitee.com>

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -11,5 +11,6 @@ public class Hello {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gttee test");
+        System.out.println("gttee test1");
     }
 }
